75fb9c3600e8f5b172e4a554568f3db45a22fff5
Jeandre Putter <81087421+JJPxtter@users.noreply.github.com>
2022-08-19 22:43:46 +0200

    Add files via upload

---
+++ FizzBuzz/ConsoleApp2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number");
+            string val =  Console.ReadLine();
+            int n = Convert.ToInt32(val);
+            for (int i = 1; i <= n; i++)
+            {
+                if (i % 3 == 0 && i % 5 == 0) 
+              {
+                    Console.WriteLine("FizzBuzz");
+                }
+                else if(i % 5 == 0)
+              {
+                    Console.WriteLine("Buzz");
+                }
+                else if (i % 3 == 0)
+                {
+                    Console.WriteLine("Fizz");
+                }
+                else {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
